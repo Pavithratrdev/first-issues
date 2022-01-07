02d3947626f5e648e197df7f63ce8c32d4ba6baa
Ashok B <lifebalance@gmail.com>
2022-01-07 22:08:24 +0530

    simple docstrings for tests

--- test/test_basic.py
+++ test/test_basic.py
@@ -12,6 +12,7 @@ example_issues = example_res['items']
 
 
 def test_fetcher():
+    """Test whether first_timer_issues are getting picked up."""
     issue_list = first_timers.get_first_timer_issues()
     assert len(issue_list) > 0
 
@@ -20,11 +21,13 @@ test_fetcher.__setattr__('__test__', False)  # Test disabled by default.
 
 
 def test_get_fresh():
+    """Test whether fresh issues are retrieved."""
     new_issues = first_timers.get_fresh(example_issues[:-1], example_issues)
     assert new_issues[0] == example_issues[-1]
 
 
 def test_humanize_url():
+    """Test whether the API response is converted for readability."""
     api_url = "https://api.github.com/repos/tidusjar/NZBDash/issues/53"
     human_url = 'https://github.com/tidusjar/NZBDash/issues/53'
     assert first_timers.humanize_url(api_url) == human_url
